5cf11f6f3f34afdba8e9669076a663e47b15a62a
Good <good@umich.edu>
2019-10-16 16:14:26 -0400

    motor functions done

--- common/mb_motor.c
+++ common/mb_motor.c
@@ -36,6 +36,13 @@ int mb_motor_init(){
 *******************************************************************************/
 int mb_motor_init_freq(int pwm_freq_hz){
     
+    printf("%d\n",rc_pwm_init(1,DEFAULT_PWM_FREQ));
+    printf("blah\n");
+    printf("%d\n",rc_gpio_init(MOT_BRAKE_EN, GPIOHANDLE_REQUEST_OUTPUT));
+    rc_gpio_init(MDIR1_CHIP, MDIR1_PIN, GPIOHANDLE_REQUEST_OUTPUT);
+    rc_gpio_init(MDIR2_CHIP, MDIR2_PIN, GPIOHANDLE_REQUEST_OUTPUT);
+    init_flag = 1;
+    mb_motor_set_all(0);
     return 0;
 }
 
@@ -49,7 +56,10 @@ int mb_motor_cleanup(){
         fprintf(stderr,"ERROR: trying cleanup before motors have been initialized\n");
         return -1;
     }
-
+    rc_pwm_cleanup(1);
+    rc_gpio_cleanup(MDIR1_CHIP, MDIR1_PIN);
+    rc_gpio_cleanup(MDIR2_CHIP, MDIR2_PIN);
+    init_flag = 0;
     return 0;
 }
 
@@ -66,6 +76,11 @@ int mb_motor_brake(int brake_en){
         return -1;
     }
 
+    if(unlikely(rc_gpio_set_value(MOT_BRAKE_EN, brake_en))){
+        fprintf(stderr,"ERROR in rc_motor_brake, failed to write to gpio pin %d,%d\n",MDIR1_CHIP, MDIR1_PIN);
+        return -1;
+    }
+   
    return 0;
 }
 
@@ -81,7 +96,8 @@ int mb_motor_disable(){
         fprintf(stderr,"ERROR: trying to disable motors before motors have been initialized\n");
         return -1;
     }
-    
+    mb_motor_set_all(0);
+
     return 0;
 }
 
@@ -101,6 +117,27 @@ int mb_motor_set(int motor, double duty){
         return -1;
     }
 
+    int a,b;
+    if(motor == 1)
+        duty=duty*MOT_1_POL;
+    else
+        duty=duty*MOT_2_POL;
+    
+    if(duty>=0.0){  a=1; b=0;}
+    else{       a=0; b=1; duty=-duty;}
+    
+    if(motor == 1)
+    {
+        rc_gpio_set_value(MDIR1_CHIP,MDIR1_PIN, a);
+        rc_gpio_set_value(MDIR1_CHIP,MDIR1_PIN, b);
+        rc_pwm_set_duty(1, 'A', duty);
+    }
+    else
+    {
+        rc_gpio_set_value(MDIR2_CHIP,MDIR2_PIN, a);
+        rc_gpio_set_value(MDIR2_CHIP,MDIR2_PIN, b);
+        rc_pwm_set_duty(1, 'B', duty);   
+    }
     return 0;
 }
 
@@ -115,7 +152,8 @@ int mb_motor_set_all(double duty){
         printf("ERROR: trying to rc_set_motor_all before they have been initialized\n");
         return -1;
     }
-    
+    mb_motor_set(1, duty);
+    mb_motor_set(2, duty);
     return 0;
 }
 
@@ -127,5 +165,7 @@ int mb_motor_set_all(double duty){
 *******************************************************************************/
 double mb_motor_read_current(int motor){
     //DRV8801 driver board CS pin puts out 500mV/A
-    return 0.0;
+    // TO DO Read value from ADC
+    return 0;
+
 }
